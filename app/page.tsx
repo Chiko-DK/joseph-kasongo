afbe6296d01097f3092e1ccb842287d77a05dae0
Chiko-DK <cdamberd@gmail.com>
2026-01-14 00:16:17 +0200

    css layout and dark/light mode feature implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/header";
-// import Hero from "@/components/hero"
+import Hero from "@/components/hero";
 // import About from "@/components/about"
 // import Skills from "@/components/skills"
 // import Projects from "@/components/projects"
@@ -12,8 +12,8 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
-      {/* <Hero />
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 md:px-8">
+      <Hero />
+      {/* <div className="max-w-4xl mx-auto px-4 sm:px-6 md:px-8">
         <div className="section-divider" />
         <About />
         <div className="section-divider" />
