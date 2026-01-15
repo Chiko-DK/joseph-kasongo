52ccd575046cbaa18728fd3240d52adbe5ddd59f
Chiko-DK <cdamberd@gmail.com>
2026-01-16 01:03:15 +0200

    Project section

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,29 @@ import Header from "@/components/header";
 import Hero from "@/components/hero";
 import About from "@/components/about"
 // import Skills from "@/components/skills"
-// import Projects from "@/components/projects"
+import Projects from "@/components/projects"
 // import Experience from "@/components/experience"
-// import Interests from "@/components/interests"
-// import Contact from "@/components/contact"
-// import Footer from "@/components/footer"
+import Contact from "@/components/contact"
+import Footer from "@/components/footer"
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
       <Hero />
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 md:px-8">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 md:px-8">
         <div className="section-divider" />
         <About />
+        <div className="section-divider" />
+        <Projects />
         {/* <div className="section-divider" />
         <Skills />
         <div className="section-divider" />
-        <Projects />
+        <Experience /> */}
         <div className="section-divider" />
-        <Experience />
-        <div className="section-divider" />
-        <Interests />
-        <div className="section-divider" />
-        <Contact /> */}
+        <Contact />
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </main>
   )
 }
